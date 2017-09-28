613018ec79d4261a2ecdfd4f99fba139604fd9ec
BT_1N3_17 <BT_1N3_17@BT_1N3_17.bktech.local>
2017-09-28 15:35:14 -0400

    project

---
+++ DavidTan/src/chatbot/Chatbot.java
@@ -0,0 +1,31 @@
+package chatbot;
+
+public class Chatbot {
+	private String username;
+	private boolean chatting;
+	private Topic david;
+	
+	public Chatbot() {
+		david = new ChatbotDavid();
+		username = "Unknown User";
+		chatting = true;
+	}
+	
+	public void startChatting() {
+		//whenever you print or get input, use these methods
+		ChatbotMain.print("Hi! I am an intelligent machine that can reposnd to your input. Tell me your name");
+		username = ChatbotMain.getInput();
+		while(chatting) {
+			ChatbotMain.print("What would yoy like to talk about?");
+			String response = ChatbotMain.getInput();
+			if(david.isTriggered(response)) {
+				chatting = false; //exits the while loop. IMPORTANT you get graded for this!
+				david.talk(response);
+			}
+			else
+			{
+				ChatbotMain.print("I'm sorry. I don't understand, I never said I was perfect.");
+			}
+		}
+	}
+}
---
+++ DavidTan/src/chatbot/ChatbotDavid.java
@@ -0,0 +1,18 @@
+package chatbot;
+
+public class ChatbotDavid implements Topic {
+
+	@Override
+	public void talk(String response) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public boolean isTriggered(String response) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+
+}
