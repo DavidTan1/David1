2cc97bbb675a287c0bb0a0ce2f8eb264887ba25b
BT_1N3_17 <BT_1N3_17@BT_1N3_17.bktech.local>
2017-09-29 14:46:01 -0400

    stuff

--- DavidTan/src/chatbot/Chatbot.java
+++ DavidTan/src/chatbot/Chatbot.java
@@ -11,12 +11,21 @@ public class Chatbot {
 		chatting = true;
 	}
 	
+	public String getUsername()
+	{
+		return username;
+	}
+	
+	public Topic getDavid() {
+		return david;
+	}
+	
 	public void startChatting() {
 		//whenever you print or get input, use these methods
 		ChatbotMain.print("Hi! I am an intelligent machine that can reposnd to your input. Tell me your name");
 		username = ChatbotMain.getInput();
 		while(chatting) {
-			ChatbotMain.print("What would yoy like to talk about?");
+			ChatbotMain.print("What would you like to talk about?");
 			String response = ChatbotMain.getInput();
 			if(david.isTriggered(response)) {
 				chatting = false; //exits the while loop. IMPORTANT you get graded for this!
